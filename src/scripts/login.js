47d8028fdd7bcea3eac5d421c0726e6283245c20
Dolfour <rodolfohenriques23@gmail.com>
2023-01-13 05:25:29 -0300

    Realizando Entrega

---
+++ src/scripts/login.js
@@ -0,0 +1,20 @@
+function openDownMenu(){
+    let change = false;
+    const menuBtn = document.querySelector(".openMenuBtn");
+    menuBtn.addEventListener("click", (event) =>{
+        event.preventDefault();
+        const buttonImage = document.querySelector(".buttonImg");
+        const menu = document.querySelector(".menuDown");
+        menu.classList.toggle("menuShow");
+        menuBtn.classList.toggle("closeMenuBtn");
+        if (change === true){
+            buttonImage.src = "/src/assets/icons/menuVector.svg";
+        
+        }else{
+            buttonImage.src = "/src/assets/icons/CloseBtn.svg";
+        }
+        change = !change;
+    });
+}
+
+openDownMenu();
